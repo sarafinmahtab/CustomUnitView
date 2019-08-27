72bb0884f32085ff23fb624522ee51c4ac349cca
mahtab <mahtab@m2sys.com>
2019-08-27 14:57:00 +0600

    Handled the bugs differently. Not solved

--- app/src/main/java/com/rnd/customunitview/CustomUnitView.kt
+++ app/src/main/java/com/rnd/customunitview/CustomUnitView.kt
@@ -6,9 +6,11 @@ import android.os.Handler
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.view.MotionEvent
-import androidx.appcompat.widget.AppCompatImageButton
-import androidx.appcompat.widget.AppCompatTextView
+import android.widget.ImageButton
+import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
+import java.math.RoundingMode
+import java.text.DecimalFormat
 
 
 class CustomUnitView : ConstraintLayout {
@@ -36,11 +38,11 @@ class CustomUnitView : ConstraintLayout {
 
     private lateinit var customUnitLayout: ConstraintLayout
 
-    private lateinit var unitDecrementImageButton: AppCompatImageButton
-    private lateinit var unitIncrementImageButton: AppCompatImageButton
+    private lateinit var decrementImageButton: ImageButton
+    private lateinit var incrementImageButton: ImageButton
 
-    private lateinit var unitAmountTextView: AppCompatTextView
-    private lateinit var unitNameTextView: AppCompatTextView
+    private lateinit var unitAmountTextView: TextView
+    private lateinit var unitNameTextView: TextView
 
     private var countHandler = Handler()
     private val counterDelay = 50L //millis
@@ -52,13 +54,13 @@ class CustomUnitView : ConstraintLayout {
     private var useFloat = false
     private var unitTypes = -1
     private var unitTypesText = ""
-    private var defaultAmount = -1F
+    private var defaultAmount = -1.0
     private var changeFactor = -1
 
-    private var currentAmount = 0F
+    private var currentAmount = 0.0
 
-    private val minRange = 0F
-    private val maxRange = 100000000F
+    private val minRange = 0.0
+    private val maxRange = 100000000.0
 
 
     private fun setAttributes(context: Context, attrs: AttributeSet?) {
@@ -71,14 +73,14 @@ class CustomUnitView : ConstraintLayout {
 
         useFloat = typedArray.getBoolean(R.styleable.CustomUnitView_useFloat, false)
         unitTypes = typedArray.getInteger(R.styleable.CustomUnitView_unitTypes, 0)
-        defaultAmount = typedArray.getFloat(R.styleable.CustomUnitView_defaultValue, -1F)
+        defaultAmount = typedArray.getFloat(R.styleable.CustomUnitView_defaultValue, -1F).toDouble().roundOffDecimal()
         changeFactor = typedArray.getInt(R.styleable.CustomUnitView_changeFactor, -1)
 
         // If amount and change factor is not set yet
         when (unitTypes) {
             0 -> {
                 // minutes
-                updateDefaultAmount(60F)
+                updateDefaultAmount(60.0)
                 updateChangeFactor(5)
 
                 unitTypesText = "minutes a day"
@@ -86,7 +88,7 @@ class CustomUnitView : ConstraintLayout {
 
             1 -> {
                 // hours
-                updateDefaultAmount(60F)
+                updateDefaultAmount(60.0)
                 updateChangeFactor(1)
 
                 unitTypesText = "hours a day"
@@ -94,7 +96,7 @@ class CustomUnitView : ConstraintLayout {
 
             2 -> {
                 // kg
-                updateDefaultAmount(68F)
+                updateDefaultAmount(68.0)
                 updateChangeFactor(2)
 
                 unitTypesText = "kilogram"
@@ -102,7 +104,7 @@ class CustomUnitView : ConstraintLayout {
 
             3 -> {
                 // lbs
-                updateDefaultAmount(145.2F)
+                updateDefaultAmount(145.2)
                 updateChangeFactor(3)
 
                 unitTypesText = "lbs"
@@ -116,8 +118,8 @@ class CustomUnitView : ConstraintLayout {
         if (changeFactor == -1) changeFactor = factor
     }
 
-    private fun updateDefaultAmount(amount: Float) {
-        if (defaultAmount == -1F) defaultAmount = amount
+    private fun updateDefaultAmount(amount: Double) {
+        if (defaultAmount == -1.0) defaultAmount = amount
     }
 
     @SuppressLint("ClickableViewAccessibility")
@@ -129,70 +131,70 @@ class CustomUnitView : ConstraintLayout {
                 true
             ) as ConstraintLayout
 
-        unitDecrementImageButton = customUnitLayout.findViewById(R.id.unitDecrementImageButton)
+        decrementImageButton = customUnitLayout.findViewById(R.id.unitDecrementImageButton)
         unitAmountTextView = customUnitLayout.findViewById(R.id.unitAmountTextView)
         unitNameTextView = customUnitLayout.findViewById(R.id.unitNameTextView)
-        unitIncrementImageButton = customUnitLayout.findViewById(R.id.unitIncrementImageButton)
+        incrementImageButton = customUnitLayout.findViewById(R.id.unitIncrementImageButton)
 
         currentAmount = defaultAmount
         unitNameTextView.text = unitTypesText
         updateAmount(currentAmount)
 
         // Decrement View listeners
-        unitDecrementImageButton.setOnLongClickListener {
+        decrementImageButton.setOnLongClickListener {
             autoDecrement = true
             countHandler.postDelayed(counterRunnable, counterDelay)
             return@setOnLongClickListener false
         }
 
-        unitDecrementImageButton.setOnTouchListener { _, p1 ->
+        decrementImageButton.setOnTouchListener { _, p1 ->
             if (p1?.action == MotionEvent.ACTION_UP && autoDecrement) {
                 autoDecrement = false
             }
             return@setOnTouchListener false
         }
 
-        unitDecrementImageButton.setOnClickListener {
+        decrementImageButton.setOnClickListener {
             decrement()
             updateAmount(currentAmount)
         }
 
 
         // Increment View listeners
-        unitIncrementImageButton.setOnLongClickListener {
+        incrementImageButton.setOnLongClickListener {
             autoIncrement = true
             countHandler.postDelayed(counterRunnable, counterDelay)
             return@setOnLongClickListener false
         }
 
-        unitIncrementImageButton.setOnTouchListener { _, p1 ->
+        incrementImageButton.setOnTouchListener { _, p1 ->
             if (p1?.action == MotionEvent.ACTION_UP && autoIncrement) {
                 autoIncrement = false
             }
             return@setOnTouchListener false
         }
 
-        unitIncrementImageButton.setOnClickListener {
+        incrementImageButton.setOnClickListener {
             increment()
             updateAmount(currentAmount)
         }
     }
 
     private fun increment() {
-        currentAmount += changeFactor
+        currentAmount = (currentAmount + changeFactor).roundOffDecimal() //TODO("Double and Float addition/subtraction has some bugs")
         if (currentAmount > maxRange) {
             currentAmount = maxRange
         }
     }
 
     private fun decrement() {
-        currentAmount -= changeFactor
+        currentAmount = (currentAmount - changeFactor).roundOffDecimal() //TODO("Double and Float addition/subtraction has some bugs")
         if (currentAmount < minRange) {
             currentAmount = minRange
         }
     }
 
-    private fun updateAmount(amount: Float) {
+    private fun updateAmount(amount: Double) {
         try {
             if (useFloat) {
                 unitAmountTextView.text = amount.toString()
@@ -247,4 +249,11 @@ class CustomUnitView : ConstraintLayout {
 
 interface UnitAmountChangeListener {
     fun onAmountChanged(amount: String)
+}
+
+
+fun Double.roundOffDecimal(): Double {
+    val df = DecimalFormat("#.#")
+    df.roundingMode = RoundingMode.CEILING
+    return df.format(this).toDouble()
 }
